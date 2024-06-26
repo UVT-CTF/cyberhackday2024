992dcfc652a18f380c312d44826e452b70f68af8
thek0d3r <horjarazvan@gmail.com>
2024-06-27 01:36:00 +0300

    Updated youtube-shorts

--- youtube-shorts/private/app.py
+++ youtube-shorts/private/app.py
@@ -7,7 +7,7 @@ def generate_random_youtube_short_url():
     short_id = ''.join(random.choices(string.ascii_letters + string.digits, k=11))
     return base_url + short_id
 
-def make_request_to_youtube_short(url):
+def make_request(url):
     try:
         print(f"Making request to: {url}")
         result = subprocess.run(['curl', '-s', '-o', '/dev/null', '-w', '%{http_code}', url], capture_output=True, text=True)
@@ -22,11 +22,15 @@ def make_request_to_youtube_short(url):
 if __name__ == "__main__":
     num_requests = 3532
 
+    for _ in range(100):
+        wiki_link = "https://en.wikipedia.org/wiki/Special:Random"
+        make_request(wiki_link)
+
     for _ in range(num_requests):
         if _ == 315:
             url = "http://www.youtube.com/shorts/&base64=SENhbXB7NTNhZTIwZTJmMDljMTM0YjIwN2Y4OWQ3MWVhYTJlMTlkOGE5NDU5ZTBjOWFiZDZmMDQxOGVlYjFiNTdhNDNkNX0="
-            make_request_to_youtube_short(url)
+            make_request(url)
             continue
 
         url = generate_random_youtube_short_url()
-        make_request_to_youtube_short(url)
+        make_request(url)
